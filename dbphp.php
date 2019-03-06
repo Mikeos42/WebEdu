a0de9a238f8b4349c37cd0aee78eeceb449ee391
Mikeos42 <miki.adamus@gmail.com>
2019-03-06 22:14:56 +0100

    Added new Database Getting better

--- dbphp.php
+++ dbphp.php
@@ -1,5 +1,28 @@
+<!doctype html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Datenbanken</title>
+</head>
+<body>
+
 <?php
+$HOST = 'sql7.freemysqlhosting.net';
+$USER = 'sql7282169';
+$PASSWORD = 'HnAwibiYGP';
+$DATABASE = 'sql7282169';
+$db = mysqli_connect($HOST, $USER, $PASSWORD, $DATABASE);
 
-
-
+if ($db) {
+    $bef = mysqli_query($db, 'SELECT * FROM benutzer;');
+    while ($satz = mysqli_fetch_array($bef)) {
+        echo $satz['benutzername'];
+        echo "<br>";
+    }
+} else {
+    echo "ES HAT NICHT FUNKTIONIERT!!";
+}
 ?>
+
+</body>
+</html>
