e7319e7e6a09b549c1f8f1899d89b40c2cb9c328
Mikeos42 <miki.adamus@gmail.com>
2019-03-06 21:51:40 +0100

    Versuche 01

--- dbphp.php
+++ dbphp.php
@@ -1,19 +1,5 @@
 <?php
-$db = mysqli_connect('localhost','root','','miki');
 
-if ($db) {
-    echo "<form method='post' action=''>
-            <input type='text' name='benutzername'>
-            <input type='text' name='passwort'>
-            <input type='submit' name='ok'>
-        </form>";
 
-    if (isset($_POST['ok'])) {
-        $sql = "SELECT * FROM benutzer";
-        mysqli_query($db, $sql);
-    }
 
-    echo "<form method='post' action=''>
-        <input type='submit' name='again'>
-        </form>";
-}
+?>
